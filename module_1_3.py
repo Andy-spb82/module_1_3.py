9cb8b86b3307500a72e011c45065cf99a9e6b34b
Andy-spb82 <Antikaspb2310@gmail.com>
2024-12-17 19:36:59 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,17 @@
+from operator import truediv
+
+name = 'andrey'
+print('name:andrey')
+print(name, type(name))
+age = 42
+print('age:42')
+print(age, type(age))
+new_age = "43"
+print('new_age:43')
+print(new_age, type(new_age))
+age = 44
+print('age:44')
+print(age, type(age))
+is_student = True
+print('is_student: True')
+print(is_student, type(is_student))
